0b5f24efb48b40a9965f701b6d72a2add7e13d8e
Sergio Saúl Torres Ibarra <50086305+sergio-torres-17@users.noreply.github.com>
2023-04-25 07:26:59 -0600

    My changes

--- JS/listaClases.js
+++ JS/listaClases.js
@@ -1,5 +1,6 @@
 function loadClases() {
-    var url = "https://localhost:7297/api/VistasPrincipalClase"
+  /**/
+      var url = "https://localhost:7297/api/VistasPrincipalClase"
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.setRequestHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IjIzMTQwMTciLCJuYmYiOjE2NzkyODc2MjEsImV4cCI6MTY4MDU4MzYyMSwiaWF0IjoxNjc5Mjg3NjIxfQ.bmXTKsBqCzBLCPnrJmobg5T06eS-K-C8ZVaQMv-FtjA");
