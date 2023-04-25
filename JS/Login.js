ad1e8fa75df515a1e6b9a2a2cca67942dbd965dd
sergio-torres-17 <torres.sergio.1am@gmail.com>
2023-04-25 00:38:40 -0600

    Modificación del login y menu principal
    Adición de secciones al hacer el login, y recuperación de JSONpara formar la pagina

--- JS/Login.js
+++ JS/Login.js
@@ -19,21 +19,23 @@ btnEnviar.onclick = function(){
 
 function codigoFuncion(){
     var peticion = new XMLHttpRequest(); //Hacer petición a backend
-    peticion.open('POST', 'http://192.168.0.173:8024/api/Login/');
+    peticion.open('POST', 'https://localhost:7297/api/Login/');
     var parametros = new FormData(); //Poner parametros
     parametros.append('Username', txtUsuario.value);
     parametros.append('Password', CryptoJS.SHA256(txtContraseña.value));//Mandar contraseña cifrada a backend
     peticion.send(parametros); //Enviar petición
-
+    console.log('Contra '+CryptoJS.SHA256(txtContraseña.value))
     //Evento que espera respuesta de API
     peticion.onload = function(){
         var objeto = JSON.parse(peticion.responseText);
+        
         //Validación de datos con respuesta de API
         if(objeto.rsp == 0){
             console.log(objeto.token);
             SetCookie('TknBrJk', objeto.token,15);
             SetCookie('userName', objeto.username, 15);
             SetCookie('nameOfUser', objeto.nombreCompleto, 15);
+            SetCookie('sections',objeto.secciones);    
             window.localStorage.setItem('img',objeto.imagen);
             console.log(window.localStorage.getItem('img'));
             swal.fire({
--- JS/index2.js
+++ JS/index2.js
@@ -1,19 +1,39 @@
 
-window.onload = function(){
+window.onload = function () {
   console.log(GetCookie('nameOfUser'));
   if (GetCookie('userName') != null) {
+    var listSections = document.getElementById('listMenu');
     var numNotifications = document.getElementById('numNotifications');
     var imgUser = document.getElementById('imgUser');
+    var secciones = JSON.parse(GetCookie('sections'));
+    crearMenu(secciones, listSections);
     txt = document.createTextNode('1');
     numNotifications.appendChild(txt);
-    imgUser.src = 'data:image/png;base64,'+window.localStorage.getItem('img');
-    console.log(GetCookie('imgBs64'));
-  }else{
+    imgUser.src = 'data:image/png;base64,' + window.localStorage.getItem('img');
+    console.log(window.localStorage.getItem('img'));
+  } else {
     location.replace('Login.html');
   }
 }
-document.getElementById('btnLogOut').addEventListener('click', function(){
+document.getElementById('btnLogOut').addEventListener('click', function () {
   DeleteCookie('TknBrJk');
   DeleteCookie('userName');
-  location.href ='index.html';
+  location.href = 'index.html';
 });
+
+function crearMenu(jsonSections, listSection) {
+  var li;
+  var a;
+  jsonSections.forEach(seccion => {
+    li = document.createElement('li');
+    a = document.createElement('a');
+    //Initialize classes CSS
+    li.className = 'nav-item';
+    a.className = 'nav-link';
+    //Creacion de ancla
+    a.href = seccion.Referencia;
+    a.innerHTML = seccion.NombreSeccion;
+    li.appendChild(a);
+    listSection.appendChild(li);
+  });
+}
