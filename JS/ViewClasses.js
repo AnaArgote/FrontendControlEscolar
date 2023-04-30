c071a5eea8c0900c713317be1c3d8bd7a403469f
sergio-torres-17 <torres.sergio.1am@gmail.com>
2023-04-30 01:35:21 -0600

    Modificacion de pagina de clases

--- JS/ViewClasses.js
+++ JS/ViewClasses.js
@@ -1,7 +1,10 @@
 //Variables for container
 var roomClass;
+var selectionRoomClass;
 var divMain = document.getElementById('mainContainer');
-var template;
+var templateAuthAdmin = document.getElementById('authAdmin');
+var templateLoadClases = document.getElementById('tmlOpenClass');
+var templateQrSpace = document.getElementById('tmlCodigoQr');
 /*
 var qrcode = new QRCode("qrcode");
 var lbl = document.getElementById('lblConteo');
@@ -44,25 +47,99 @@ function countListStudents(){
     students = '';
   }
 }
-
-/****************Template Open Class****************/
-function loadTemplateOpenClass(){
-  template = document.getElementById('tmlOpenClass');
-  const clone = template.content.cloneNode(true);
-  divMain.append(clone);
+/****************Authenticate admin******************/
+function loadTemplateAuthenticateAdmin(){
+  templateAuthAdmin.style.display = 'block';
 }
+function loginAdmin(){
+  var txtUsuario = document.getElementById('txtUserAdmin');
+  var txtPassword = document.getElementById('txtPasswordAdmin');
+  var request = new XMLHttpRequest();
+  var prms = new FormData();
+  prms.append('username', txtUsuario.value);
+  prms.append('password', CryptoJS.SHA256(txtPassword.value));
+  request.open('POST', 'https://localhost:7297/api/Login');
+  request.send(prms);
+  request.onload = function(){
+    var response = JSON.parse(request.responseText);
+    SetCookie('tknAuth', response.token);
+    SetCookie('ExistAuth', 1);
+    if(response.rsp == 0 && response.userType == 1000){ 
+      Swal.fire(
+      'Correcto',
+      'Autenticado correctamente',
+      'success'
+    )
+      loadTemplateOpenClass();
+    }else{
+      Swal.fire(
+        'Error',
+        'Usuario o contraseña incorrectos',
+        'error'
+      )
+    }
+  }
+}
+/****************Template Open Class****************/
 function loadClassRooms() {
-  document.coo
+  
+  templateLoadClases.style.display = 'block';
+}
+function loadTemplateOpenClass(){
+  templateAuthAdmin.style.display = 'none';
+  templateLoadClases.style.display = 'block';
+  loadExistingRoomClases();
+}
+function loadExistingRoomClases(){
+  selectionRoomClass = document.getElementById('nombreAula');
+  var requestRooms = new XMLHttpRequest();
+  requestRooms.open('GET', 'http://localhost:8024/api/VisorAulas');
+  requestRooms.setRequestHeader('Authorization', 'Bearer '+GetCookie('tknAuth'));
+  requestRooms.send();
+  
+  let optionObj = document.createElement('option');
+  optionObj.text = optionObj.value = 'Selecciona una opción...';
+  selectionRoomClass.appendChild(optionObj);
+  requestRooms.onload = function(){
+    console.log(requestRooms.responseText);
+    var rooms = JSON.parse(requestRooms.responseText);  
+    rooms.forEach(room => {
+        optionObj = document.createElement('option');
+        optionObj.text =  room.nombre;
+        selectionRoomClass.appendChild(optionObj);
+    });
+}  
+}
+function saveRoomClassName() {
+  if (selectionRoomClass.selectedIndex > 0) {
+    console.log(selectionRoomClass[selectionRoomClass.selectedIndex].text);  
+  }else{
+    Swal.fire({
+      icon: 'error',
+      title: '¡Error!',
+      text: 'La opción seleccionada no es valida'
+    });
+  }
+}
+function saveFilesRoomClass(){
+  
 }
 /****************Template Open Class****************/
 //Generic
 function mainMethod(){
+  //DeleteCookie('ExistAuth');
+  //DeleteCookie('tknAuth');
   roomClass = GetCookie('nameRoomClass');
+  auth = GetCookie('ExistAuth');
   if(roomClass != null){
     
   }else{
-    loadTemplateOpenClass();
-    console.log('No existe Cookie');
+    if(auth != null){
+      loadTemplateOpenClass();
+    }else{
+      loadTemplateAuthenticateAdmin();
+    }    
+    
   }
 }
 window.onload=function() {
